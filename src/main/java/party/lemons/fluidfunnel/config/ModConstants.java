6b72a140da4d166e788053ddda852b35c2672d91
Sam <lemonverse@gmail.com>
2018-11-08 13:59:58 +1300

    push for backup

--- src/main/java/party/lemons/fluidfunnel/config/ModConstants.java
+++ src/main/java/party/lemons/fluidfunnel/config/ModConstants.java
@@ -7,5 +7,5 @@ public class ModConstants
 {
 	public static final String MODID = "fluidfunnel";
 	public static final String NAME = "Fluid Funnel";
-	public static final String VERSION = "1.2.0";
+	public static final String VERSION = "1.3.0";
 }
